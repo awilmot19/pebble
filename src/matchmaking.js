c67ee68f8df02ea030c00f658a0cb402e0acd269
Connor-Fraser <connor.fraser4@gmail.com>
2015-03-29 08:27:08 -0400

    newisgood

--- src/matchmaking.js
+++ src/matchmaking.js
@@ -19,6 +19,18 @@ var xhrRequest = function (url, type, data, callback) {
   xhr.send(JSON.stringify(data));
 };
 
+var xhrRequestAsync = function (url, type, data, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  
+  xhr.open(type, url, true);
+  xhr.setRequestHeader('Content-type', 'application/json');
+  xhr.send(JSON.stringify(data));
+};
+
+
 function locationSuccess(pos) {
   // Construct URL
   var url = baseUrl + register;
@@ -48,9 +60,9 @@ function locationSuccess(pos) {
       
     // Assemble dictionary using our keys
     var dictionary = {
-      'KEY_START': start,
       'KEY_PLAYER_NUM': player_num,
-      'KEY_OPPONENT_NUM': opponent_num
+      'KEY_OPPONENT_NUM': opponent_num,
+      'KEY_START': start
     };
 
     // Send to Pebble
@@ -93,7 +105,7 @@ Pebble.addEventListener('appmessage',
     if(e.payload.KEY_PLAY_POS){
       //send position
       url = baseUrl + give_pos;
-      xhrRequest(url, 'POST', { "x_pos" : e.payload.KEYPLAY_POS }, function(responseText){
+      xhrRequestAsync(url, 'POST', { "x_pos" : e.payload.KEYPLAY_POS }, function(responseText){
         console.log("give_pos response text: " + responseText);
         
         url = baseUrl + get_pos;
